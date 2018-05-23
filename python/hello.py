600f66e7527ae0207e2a377d68d04d492baa5c80
ViniKim <ashaelf70@google.com>
2018-05-23 12:38:49 +0900

    renew hello.py: to Fizzbuzz

--- python/hello.py
+++ python/hello.py
@@ -1,3 +1,12 @@
-for i in range(1, 5+1):
-    if i % 2 != 0:
-        print("hello python!")
+for i in range(101):
+    if i % 15 == 0:
+        print("Fizzbuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+
+
